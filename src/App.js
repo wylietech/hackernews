94a4d8e890e439a0317eef232acfe038d3da7d1a
Matthew Wylie <matthew.t.wylie@googlemail.com>
2018-04-30 11:13:13 +0100

    Modified the app to work with real Hackernews API

--- src/App.js
+++ src/App.js
@@ -2,24 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux2',  
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const DEFAULT_QUERY = 'redux';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+console.log(url);
 
 const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
@@ -29,10 +18,27 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { list, searchTerm : '' };
+    this.state = { 
+      searchTerm : DEFAULT_QUERY,
+      result : null
+    };
 
-    this.onDismiss = this.onDismiss.bind(this);
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+    
+  }
+
+  setSearchTopStories(result) {
+    this.setState( {result} );
+  }
+
+  componentDidMount() {
+    const {searchTerm} = this.state;
+    fetch (`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+    .then(response => response.json())
+    .then(result => this.setSearchTopStories(result))
+    .catch(error => error);
   }
 
   onSearchChange(event) {
@@ -48,7 +54,12 @@ class App extends Component {
   }
 
   render() {
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+
+    if (!result) {
+      return null;
+    }
+
     return (
       <div className="page">
         <div className="interactions">
@@ -57,7 +68,7 @@ class App extends Component {
             onChange={this.onSearchChange}
           />
           <Table
-            list={list}
+            list={result.hits}
             pattern={searchTerm}
             onDismiss={this.onDismiss}
           />
